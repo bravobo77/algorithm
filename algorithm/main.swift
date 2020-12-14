29c7aceb9635ac4ecf166a8da5b6c596311caf14
bravobo77 <a950067300@gmail.com>
2020-12-14 18:57:20 +0900

    baekjoon q.10869

--- algorithm/main.swift
+++ algorithm/main.swift
@@ -8,33 +8,16 @@
 
 import Foundation
 
-var input = 0
-var arr: Array<Int> = Array()
-//var arr2: Array<Int> = Array()
+let num = readLine()
 
-for _ in 0..<3 {
-    input = Int(readLine()!)!
-    arr.append(input)
-}
-
-var a: Int = arr[0]
-var b: Int = arr[1]
-var c: Int = arr[2]
-
-let result = a*b*c
-
-var count: [Int] = Array.init(repeating: 0, count: 10)
-let results = String(result)
-
-results.forEach{ i in
-    let j = Int(String(i))
-    for k in 0..<10 {
-        if j == k {
-            count[k] += 1
-        }
-    }
-}
-
-count.forEach {
-    print($0)
+if let number = num {
+    let array = number.components(separatedBy: " ")
+    let a = Int(array[0])!
+    let b = Int(array[1])!
+    
+    print(a + b)
+    print(a - b)
+    print(a * b)
+    print(a / b)
+    print(a % b)
 }
