59e9e70cd99453f0568a73821b449c04ab0f115e
bravobo77 <a95067300@gmail.com>
2021-01-13 13:01:02 +0900

    baeckjoon q.10871 고차함수 ver

--- algorithm/main.swift
+++ algorithm/main.swift
@@ -11,45 +11,16 @@ import Foundation
 var number = readLine()!
 var arr = number.split(separator: " ")
 
-var inputN = Int(arr[0])!
 var inputX = Int(arr[1])!
 
-//print(inputN,inputX)
-
-
-var inputArr: [Int] = []
-
-let randomN = 0
-
-for _ in 0..<inputN {
-    inputArr.append(randomN)
-}
-
 var number2 = readLine()!
-var arr2 = number2.split(separator: " ").map({Int($0)!})
+var result = number2.split(separator: " ")
+    .map({Int($0)!})
+    .filter{$0<inputX}
+    .map{"\($0)"}
+    .joined(separator: " ")
 
-
-if arr2.count != inputArr.count {
-    print("숫자의 갯수를 정확히 입력해주세요")
-    exit(0)
-}
-
-print(arr2)
-
-var result: [Int] = []
-
-var s = 0
-
-for i in arr2 {
-//    print(i)
-    if i < inputX {
-        result.append(i)
-//        s += 1
-    }
-    
-}
-
-print(result.map{"\($0)"}.joined(separator: " "))
+print(result)
 
 
 
